5236cd6c5b8865f57e41bcacb9ea479bffcfb483
rahulshewani <31309190+rahulshewani@users.noreply.github.com>
2017-12-01 18:56:55 +0530

    Add files via upload

---
+++ MANAS1.ino
@@ -0,0 +1,44 @@
+
+int i=9;
+int j;
+int inpin=7;
+int inpin1=8;
+long time=0;
+long debounce=200;
+int previous=LOW;
+int previous1=LOW;
+int state=HIGH;
+int state1=HIGH;
+int f=0;
+
+void setup()
+{
+pinMode(inpin,INPUT);
+pinMode(inpin1,INPUT);
+Serial.begin(9600);
+}
+
+void loop()
+{
+  int a=digitalRead(inpin);
+  if(a==HIGH && previous==LOW && millis() -time > debounce)
+  {
+if(state==HIGH)
+{
+i--;
+//state=LOW;
+Serial.println(i);
+delay(50);
+if(i==3)
+{
+  i=6;
+}
+}
+time=millis();
+
+}
+}
+ 
+
+
+
---
+++ MANAS2.ino
@@ -0,0 +1,34 @@
+int i=0;
+int inpin=7;
+long time=0;
+long debounce=200;
+int previous=LOW;
+int state=HIGH;
+
+void setup()
+{
+pinMode(inpin,INPUT);
+
+Serial.begin(9600);
+}
+
+void loop()
+{
+  int a=digitalRead(inpin);
+  if(a==HIGH && previous==LOW && millis() -time > debounce)
+  {
+if(state==HIGH)
+{
+i++;
+Serial.println(i);
+delay(50);
+if(i==9)
+{
+  i=0;
+}
+}
+time=millis();
+
+}
+}
+ 
